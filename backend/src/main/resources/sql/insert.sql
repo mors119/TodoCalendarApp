652cbfab78bc15092ca234d6bd806480e8ac729b
heesk0223@gmail.com <heesk0223@gmail.com>
2025-02-19 18:50:01 +0900

    Add all changes

--- backend/src/main/resources/sql/insert.sql
+++ backend/src/main/resources/sql/insert.sql
@@ -3,7 +3,6 @@ VALUES
     ('제출기한 확인', '과제 제출 기한은 최대 7일입니다.', NOW(), NOW() + INTERVAL 7 DAY, 0, 'A', '과제 준비', ':userId', NOW(), NOW()),
     ('과제 평가 및 인터뷰 준비', '과제 제출 후 최대 7일 내 평가 및 인터뷰 진행', NOW(), NOW() + INTERVAL 7 DAY, 0, 'A', '과제 준비', ':userId', NOW(), NOW()),
     ('인터뷰 준비', '제출한 과제를 기반으로 질문하며, 인성 위주로 진행됨', NOW(), NOW() + INTERVAL 7 DAY, 0, 'B', '인터뷰 준비', ':userId', NOW(), NOW()),
-    ('프론트엔드 + 백엔드 수행 여부 확인', '프론트엔드 & 백엔드 과제를 함께 수행 시 가산점 부여', NOW(), NOW() + INTERVAL 7 DAY, 0, 'A', '과제 준비', ':userId', NOW(), NOW()),
     ('GitHub Repo 생성', 'GitHub에 레포지토리를 생성하고 과제 소스를 관리', NOW(), NOW() + INTERVAL 3 DAY, 0, 'A', '개발 준비', ':userId', NOW(), NOW()),
     ('README.md 작성', '프로젝트 설명, 실행 방법, 주요 라이브러리 및 API 명세 포함', NOW(), NOW() + INTERVAL 3 DAY, 0, 'A', '개발 문서화', ':userId', NOW(), NOW()),
     ('할일 (ToDo) 또는 일정 (Calendar) 앱 개발', 'React + Ant Design을 사용하여 개발 진행', NOW(), NOW() + INTERVAL 7 DAY, 0, 'A', '프론트엔드 개발', ':userId', NOW(), NOW()),
